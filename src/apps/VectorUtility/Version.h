fbded8d96972267b7416c46a9198cd902592388d
TOtto-EclipseIR <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-01-25 15:40:40 -0800

    Start v1.02D

--- src/apps/VectorUtility/Version.h
+++ src/apps/VectorUtility/Version.h
@@ -4,7 +4,7 @@
 
 #define VER_MAJOR   1
 #define VER_MINOR   2
-#define VER_RELEASE 3
+#define VER_RELEASE 4
 #define VER_BRANCH  0 // 2=dev+role
 #define VER_BUILD   0
-#define VER_STRING  "v1.02C"
+#define VER_STRING  "v1.02D"
