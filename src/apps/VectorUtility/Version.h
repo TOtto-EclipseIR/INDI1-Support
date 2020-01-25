96d6425cafc7ff9163c4f40970de1a5552cf9017
TOtto-EclipseIR <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-01-25 15:31:43 -0800

    Finalizing Search Result matrix image.

--- src/apps/VectorUtility/Version.h
+++ src/apps/VectorUtility/Version.h
@@ -5,6 +5,6 @@
 #define VER_MAJOR   1
 #define VER_MINOR   2
 #define VER_RELEASE 3
-#define VER_BRANCH  2 // dev+role
+#define VER_BRANCH  0 // 2=dev+role
 #define VER_BUILD   0
-#define VER_STRING  "v1.02C+2"
+#define VER_STRING  "v1.02C"
