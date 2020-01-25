52f265d26c1b2d52edb2bf23d89d68490a960ccd
TOtto-EclipseIR <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-01-25 15:43:17 -0800

    Move v1.02D back to dev+role branch.

--- src/apps/VectorUtility/Version.h
+++ src/apps/VectorUtility/Version.h
@@ -5,6 +5,6 @@
 #define VER_MAJOR   1
 #define VER_MINOR   2
 #define VER_RELEASE 4
-#define VER_BRANCH  0 // 2=dev+role
+#define VER_BRANCH  2 // 2=dev+role
 #define VER_BUILD   0
-#define VER_STRING  "v1.02D"
+#define VER_STRING  "v1.02D+2"
